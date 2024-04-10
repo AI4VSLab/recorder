f52b766cddbbb9ae43b1b806b4baaaa69a708c8b
sc4789 <sc4789@columbia.edu>
2024-04-10 15:55:36 +0000

    updated script for timestamps

--- audio/script_new.py
+++ audio/script_new.py
@@ -2,32 +2,56 @@ import os
 import json
 from subprocess import Popen, PIPE
 import pandas as pd
+from google.cloud import speech
+
+
 
 def convert_to_flac(wav_file):
     flac_file = os.path.splitext(wav_file)[0] + ".flac"
-    command = f"ffmpeg -i {wav_file} {flac_file}"
+    command = f"ffmpeg -i {wav_file} -ar 44100 {flac_file}"
     process = Popen(command.split(), stdout=PIPE, stderr=PIPE)
     output, error = process.communicate()
     return flac_file if os.path.exists(flac_file) else flac_file
 
-def json_csv(json_output, file_name): 
-    # data = json.loads(json_output)
+def json_csv(result, file_name, alt_num): 
 
-    # Concatenate transcripts
-    transcript = ' '.join([result['alternatives'][0]['transcript'] for result in json_output['results']])
-
-    # Create DataFrame
-    df_iter = pd.DataFrame({'File': [file_name], 'Transcript': [transcript]})
+    alternative = result.alternatives[0]
+    result_timestamps_string = ""
+    for word_info in alternative.words:
+        word = word_info.word
+        start_time = word_info.start_time
+        end_time = word_info.end_time
+        timestamp_info = f"Word: {word}, start_time: {start_time.total_seconds()}, end_time: {end_time.total_seconds()}\n"
+        result_timestamps_string = result_timestamps_string + timestamp_info
+    file_name = file_name + "_" + "alternative" + "_" + str(alt_num)
+    df_iter = pd.DataFrame({'File': [file_name], 'Transcript': [alternative.transcript], "Confidence": [alternative.confidence], "TimeStamps" : [result_timestamps_string]})
 
     return df_iter
 
 
 
 def recognize_audio(file_path):
-    command = f"gcloud ml speech recognize {file_path} --language-code=en-US"
-    process = Popen(command.split(), stdout=PIPE, stderr=PIPE)
-    output, error = process.communicate()
-    return output.decode("utf-8")
+   
+    with open(file_path, "rb") as audio_file:
+        file_content = audio_file.read()
+
+    client = speech.SpeechClient()
+
+
+    audio = speech.RecognitionAudio(content=file_content)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=44100,
+        language_code="en-US",
+        enable_word_time_offsets=True,
+    )
+
+    operation = client.long_running_recognize(config=config, audio=audio)
+
+    print("Waiting for operation to complete...")
+    result = operation.result(timeout=100000)
+
+    return result
 
 def main():
     output_file = "output.txt"
@@ -44,16 +68,12 @@ def main():
                 print(flac_file)
                 recognition_result = recognize_audio(flac_file)
                 f_out.write(f"File: {filename}\n")
+                alt_num = 0
                 try:
-                    json_output = json.loads(recognition_result)
-                    df_iter = json_csv(json_output, filename)
-                    # df_all = df_all.append(df_iter, ignore_index=True)
-                    print(df_iter)
-                    df_all = pd.concat([df_all, df_iter], ignore_index=True)
-
-
-
-                    # f_out.write(json.dumps(json_output, indent=4))
+                    for result in recognition_result.results: 
+                        df_iter = json_csv(result, filename, alt_num)                        
+                        df_all = pd.concat([df_all, df_iter], ignore_index=True)
+                        alt_num+=1
                 except json.JSONDecodeError:
                     f_out.write("Error: Unable to decode JSON output")
                 f_out.write("\n\n")
